2137c8619375f4bbe632143ad71759d4121266c8
Al-Rou <roal.24.alro@gmail.com>
2021-03-02 22:55:39 -0500

    Party two complete

--- src/SecondParty.java
+++ src/SecondParty.java
@@ -1,2 +1,45 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Scanner;
+
 public class SecondParty {
+        public static void main(String[] args)
+        {
+            //A connection is established and the first number is received from Player One
+            try {
+                DatagramSocket datagramSocket = new DatagramSocket(5558);
+                //This loop continues sending and receiving data until the messaging finishes
+                while (true){
+                    //Receiving data
+                    ByteBuffer byteBuffer2 = ByteBuffer.allocate(1024);
+                    byteBuffer2.order(ByteOrder.BIG_ENDIAN);
+                    DatagramPacket datagramPacket2 = new DatagramPacket(byteBuffer2.array(),
+                            byteBuffer2.array().length);
+                    datagramSocket.receive(datagramPacket2);
+                    String receivedMess = "";
+                    receivedMess += byteBuffer2.getChar();
+                    System.out.println(receivedMess);
+
+                    Scanner in = new Scanner(System.in);
+                    String sendingMess = in.nextLine();
+                    ByteBuffer byteBuffer3 = ByteBuffer.allocate(1024);
+                    byteBuffer3.order(ByteOrder.BIG_ENDIAN);
+                    for (int j=0; j < sendingMess.length(); j++) {
+                        byteBuffer3.putChar(sendingMess.charAt(j));
+                    }
+                    DatagramPacket datagramPacket3 = new DatagramPacket(byteBuffer3.array(),
+                            byteBuffer3.position(), datagramPacket2.getAddress(),
+                            datagramPacket2.getPort());
+                    datagramSocket.send(datagramPacket3);
+                }
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+
+
 }
