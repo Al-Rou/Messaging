d54b483ed450ec9d661ff5a5c669dff72ad2671d
Al-Rou <roal.24.alro@gmail.com>
2021-03-06 14:02:51 -0500

    Thread done!

--- src/FirstParty.java
+++ src/FirstParty.java
@@ -21,59 +21,79 @@ public class FirstParty {
                 DatagramSocket datagramSocket = new DatagramSocket(5557);
                 //This loop continues sending and receiving data until the messaging finishes
                 do {
-                    System.out.print("Your message: ");
-                    Scanner in = new Scanner(System.in);
-                    List<String> messageList = new ArrayList<>();
-                    while (true){
-                        String message = in.nextLine();
-                        if(message.equals("&"))
-                        {
-                            break;
+                    Thread threadSend = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                //System.out.print("Your message: ");
+                                Scanner in = new Scanner(System.in);
+                                List<String> messageList = new ArrayList<>();
+                                while (true){
+                                    String message = in.nextLine();
+                                    if(message.equals("&"))
+                                    {
+                                        break;
+                                    }
+                                    else {
+                                        messageList.add(message);
+                                    }
+                                }
+                                //Time of sending message is stamped
+                                Date dateOfSending = new Date();
+                                DateFormat dateFormatForSent = new SimpleDateFormat("HH:mm:ss");
+                                String timeOfSending = dateFormatForSent.format(dateOfSending.getTime());
+                                System.out.println("(sent at " + timeOfSending + ")");
+                                //Sending data
+                                ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                                byteBuffer.order(ByteOrder.BIG_ENDIAN);
+                                for (int i=0; i < messageList.size(); i++)
+                                {
+                                    for (int j=0; j < messageList.get(i).length(); j++)
+                                    {
+                                        byteBuffer.putChar(messageList.get(i).charAt(j));
+                                    }
+                                    byteBuffer.putChar('\n');
+                                }
+                                DatagramPacket datagramPacket = new DatagramPacket(byteBuffer.array(), byteBuffer.position(),
+                                        InetAddress.getLocalHost(), 5558);
+                                datagramSocket.send(datagramPacket);
+                            }catch (Exception e)
+                            {
+                                e.printStackTrace();
+                            }
                         }
-                        else {
-                            messageList.add(message);
+                    });
+                    Thread threadReceive = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                //Receiving data
+                                ByteBuffer byteBufferResponse = ByteBuffer.allocate(1024);
+                                byteBufferResponse.order(ByteOrder.BIG_ENDIAN);
+                                DatagramPacket datagramPacketRes = new DatagramPacket(byteBufferResponse.array(),
+                                        byteBufferResponse.array().length);
+                                datagramSocket.receive(datagramPacketRes);
+                                String receivedMessage = "";
+                                do {
+                                    receivedMessage += byteBufferResponse.getChar();
+                                }while (byteBufferResponse.hasRemaining());
+                                Date dateOfReceiving = new Date();
+                                Timestamp timestamp = new Timestamp(dateOfReceiving.getTime());
+                                System.out.print("PartyTwo: ");
+                                System.out.print(receivedMessage);
+                                String timeString = timestamp.toString();
+                                String timeOfReceiving = timeString.substring(11, 19);
+                                System.out.println(" (" + timeOfReceiving + ")");
+                            }catch (Exception e)
+                            {
+                                e.printStackTrace();
+                            }
                         }
-                    }
-                    //Time of sending message is stamped
-                    Date dateOfSending = new Date();
-                    DateFormat dateFormatForSent = new SimpleDateFormat("HH:mm:ss");
-                    String timeOfSending = dateFormatForSent.format(dateOfSending.getTime());
-                    System.out.println("(sent at " + timeOfSending + ")");
-                    //Sending data
-                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-                    byteBuffer.order(ByteOrder.BIG_ENDIAN);
-                    /*for (int j=0; j < message.length(); j++) {
-                        byteBuffer.putChar(message.charAt(j));
-                    }*/
-                    for (int i=0; i < messageList.size(); i++)
-                    {
-                        for (int j=0; j < messageList.get(i).length(); j++)
-                        {
-                            byteBuffer.putChar(messageList.get(i).charAt(j));
-                        }
-                        byteBuffer.putChar('\n');
-                    }
-                    DatagramPacket datagramPacket = new DatagramPacket(byteBuffer.array(), byteBuffer.position(),
-                            InetAddress.getLocalHost(), 5558);
-                    datagramSocket.send(datagramPacket);
-                    //Receiving data
-                    ByteBuffer byteBufferResponse = ByteBuffer.allocate(1024);
-                    byteBufferResponse.order(ByteOrder.BIG_ENDIAN);
-                    DatagramPacket datagramPacketRes = new DatagramPacket(byteBufferResponse.array(),
-                            byteBufferResponse.array().length);
-                    datagramSocket.receive(datagramPacketRes);
-                    String receivedMessage = "";
-                    do {
-                        receivedMessage += byteBufferResponse.getChar();
-                    }while (byteBufferResponse.hasRemaining());
-                    Date dateOfReceiving = new Date();
-                    Timestamp timestamp = new Timestamp(dateOfReceiving.getTime());
-                    System.out.print("PartyTwo: ");
-                    System.out.print(receivedMessage);
-                    String timeString = timestamp.toString();
-                    String timeOfReceiving = timeString.substring(11, 19);
-                    System.out.println(" (" + timeOfReceiving + ")");
-
+                    });
+                    threadSend.start();
+                    threadReceive.start();
+                    threadSend.join();
+                    threadReceive.join();
                 }while (true);
             }catch (Exception e)
             {
--- src/SecondParty.java
+++ src/SecondParty.java
@@ -1,5 +1,6 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.sql.Timestamp;
@@ -11,6 +12,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class SecondParty {
+    private static InetAddress packetAddress;
+    private static int packetPort;
         public static void main(String[] args)
         {
             //A connection is established and the first number is received from Player One
@@ -23,55 +26,80 @@ public class SecondParty {
                         " (type \"&\" at the end of your message in order to send)");
                 //This loop continues sending and receiving data until the messaging finishes
                 while (true){
-                    //Receiving data
-                    String receivedMess = "";
-                    ByteBuffer byteBuffer2 = ByteBuffer.allocate(1024);
-                    byteBuffer2.order(ByteOrder.BIG_ENDIAN);
-                    DatagramPacket datagramPacket2 = new DatagramPacket(byteBuffer2.array(),
-                            byteBuffer2.array().length);
-                    datagramSocket.receive(datagramPacket2);
-
-                    do {
-                        receivedMess += byteBuffer2.getChar();
-                    }while (byteBuffer2.hasRemaining());
-                    //Time of receiving message is stamped
-                    Date dateOfReceiving = new Date();
-                    DateFormat dateFormatForReceived = new SimpleDateFormat("HH:mm:ss");
-                    String timeToShow = dateFormatForReceived.format(dateOfReceiving.getTime());
-                    System.out.print("PartyOne: ");
-                    System.out.print(receivedMess);
-                    System.out.println(" (" + timeToShow + ")");
-                    //Sending data
-                    System.out.print("Your message: ");
-                    Scanner in = new Scanner(System.in);
-                    List<String> messageList = new ArrayList<>();
-                    while (true){
-                        String message = in.nextLine();
-                        if(message.equals("&"))
-                        {
-                            break;
+                    Thread threadReceive = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                //Receiving data
+                                String receivedMess = "";
+                                ByteBuffer byteBuffer2 = ByteBuffer.allocate(1024);
+                                byteBuffer2.order(ByteOrder.BIG_ENDIAN);
+                                DatagramPacket datagramPacket2 = new DatagramPacket(byteBuffer2.array(),
+                                        byteBuffer2.array().length);
+                                datagramSocket.receive(datagramPacket2);
+                                packetAddress = datagramPacket2.getAddress();
+                                packetPort = datagramPacket2.getPort();
+                                do {
+                                    receivedMess += byteBuffer2.getChar();
+                                }while (byteBuffer2.hasRemaining());
+                                //Time of receiving message is stamped
+                                Date dateOfReceiving = new Date();
+                                DateFormat dateFormatForReceived = new SimpleDateFormat("HH:mm:ss");
+                                String timeToShow = dateFormatForReceived.format(dateOfReceiving.getTime());
+                                System.out.print("PartyOne: ");
+                                System.out.print(receivedMess);
+                                System.out.println(" (" + timeToShow + ")");
+                            }catch (Exception e)
+                            {
+                                e.printStackTrace();
+                            }
                         }
-                        else {
-                            messageList.add(message);
+                    });
+                    Thread threadSend = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                //Sending data
+                                //System.out.print("Your message: ");
+                                Scanner in = new Scanner(System.in);
+                                List<String> messageList = new ArrayList<>();
+                                while (true){
+                                    String message = in.nextLine();
+                                    if(message.equals("&"))
+                                    {
+                                        break;
+                                    }
+                                    else {
+                                        messageList.add(message);
+                                    }
+                                }
+                                Date dateOfSending = new Date();
+                                Timestamp timestamp = new Timestamp(dateOfSending.getTime());
+                                String sentTime = timestamp.toString();
+                                String sentTimeToShow = sentTime.substring(11, 19);
+                                System.out.println("(sent at " + sentTimeToShow + ")");
+                                ByteBuffer byteBuffer3 = ByteBuffer.allocate(1024);
+                                byteBuffer3.order(ByteOrder.BIG_ENDIAN);
+                                for (int i=0; i < messageList.size(); i++) {
+                                    for (int j = 0; j < messageList.get(i).length(); j++) {
+                                        byteBuffer3.putChar(messageList.get(i).charAt(j));
+                                    }
+                                    byteBuffer3.putChar('\n');
+                                }
+                                DatagramPacket datagramPacket3 = new DatagramPacket(byteBuffer3.array(),
+                                        byteBuffer3.position(), packetAddress,
+                                        packetPort);
+                                datagramSocket.send(datagramPacket3);
+                            }catch (Exception e)
+                            {
+                                e.printStackTrace();
+                            }
                         }
-                    }
-                    Date dateOfSending = new Date();
-                    Timestamp timestamp = new Timestamp(dateOfSending.getTime());
-                    String sentTime = timestamp.toString();
-                    String sentTimeToShow = sentTime.substring(11, 19);
-                    System.out.println("(sent at " + sentTimeToShow + ")");
-                    ByteBuffer byteBuffer3 = ByteBuffer.allocate(1024);
-                    byteBuffer3.order(ByteOrder.BIG_ENDIAN);
-                    for (int i=0; i < messageList.size(); i++) {
-                        for (int j = 0; j < messageList.get(i).length(); j++) {
-                            byteBuffer3.putChar(messageList.get(i).charAt(j));
-                        }
-                        byteBuffer3.putChar('\n');
-                    }
-                    DatagramPacket datagramPacket3 = new DatagramPacket(byteBuffer3.array(),
-                            byteBuffer3.position(), datagramPacket2.getAddress(),
-                            datagramPacket2.getPort());
-                    datagramSocket.send(datagramPacket3);
+                    });
+                    threadReceive.start();
+                    threadSend.start();
+                    threadReceive.join();
+                    threadSend.join();
                 }
             }catch (Exception e)
             {
